dedbd2dc35b0065b5414c21a9f073603fce27b57
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2022-06-17 13:12:49 -0300

    Criando matrizes de acordo com predicate e letras aleatórias

--- jogoMemoria/jogoaMemomoria.py
+++ jogoMemoria/jogoaMemomoria.py
@@ -1,3 +1,9 @@
+import random
+import string
+
+alafabeto = string.ascii_uppercase
+
+
 def get(linhaIndex, colunaIndex, matrix):
     for l in range(len(matrix)):
         if l == linhaIndex:
@@ -5,14 +11,48 @@ def get(linhaIndex, colunaIndex, matrix):
                 if c == colunaIndex:
                     return matrix[l][c]
 
+
 def imprimirMatrizOculta(matriz):
     print("---Matriz de exibição--- \n %s" % matriz)
 
-def iniciar(matrizOculta,matriz,paresDeRespostas):
+def frequenciaLetra(letra,matriz):
+    vezes=0
+    for l in range(len(matriz)):
+        for c in range(len(matriz[l])):
+            if matriz[l][c] == letra:
+                vezes = vezes +1
+    return vezes
+def getMatrizCombinacoes(matriz):
+    combinacoes:int = 0
+    for l in range(len(matriz)):
+        for c in range(len(matriz[l])):
+            if frequenciaLetra(matriz[l][c],matriz) == 2:
+                combinacoes=combinacoes+1
+    return combinacoes
+
+def criaMatriz(numero_linhas, numero_colunas, oculta):
+    matriz = []
+    for l in range(numero_linhas):
+        matriz.append([])
+        for c in range(numero_colunas):
+            if oculta == 1:
+                matriz[l].append("#")
+            else:
+                matriz[l].append(geraCharacter())
+
+    return matriz
+
+
+def geraCharacter():
+    return ''.join(random.choice(alafabeto) for i in range(1))
+
+
+def iniciar(matrizOculta, matriz):
     vezesMatrizExibidas: int = 0
     respostasCertas: int = 0
-    totalRespostasCertas: int = paresDeRespostas
-    while respostasCertas < totalRespostasCertas:
+    totalCombinacoes:int= getMatrizCombinacoes(matriz)
+    print("Existem %s combinações. Acerte todas para completar o jogo!"%totalCombinacoes)
+    while respostasCertas < totalCombinacoes:
         if vezesMatrizExibidas <= 2:
             exibirResposta: int = int((input("Deseja exibir matriz original?1-sim, 2-não")))
             if exibirResposta == 1:
@@ -21,7 +61,7 @@ def iniciar(matrizOculta,matriz,paresDeRespostas):
         linhaPrimeiroQuadrante: int = int(input("Digite a linha do primeiro quadrante. Baseado em 0!"))
         colunaPrimeiroQuadrante: int = int(input("Digite a coluna do primeiro quadrante. Baseado em 0!"))
         letra = get(linhaPrimeiroQuadrante, colunaPrimeiroQuadrante, matriz)
-        print("Letra %s " % letra)
+        print("Letra %s! " % letra)
         linhaSegundoQuadrante: int = int(input("Digite a linha do segundo quadrante. Baseado em 0!"))
         colunaSegundoQuadrante: int = int(input("Digite a coluna do segundo quadrante. Baseado em 0!"))
         letraSegundoQuadrante = get(linhaSegundoQuadrante, colunaSegundoQuadrante, matriz)
@@ -31,35 +71,36 @@ def iniciar(matrizOculta,matriz,paresDeRespostas):
             matrizOculta[linhaSegundoQuadrante][colunaSegundoQuadrante] = letraSegundoQuadrante
             print("---Matriz de exibição--- \n %s" % matrizOculta)
             respostasCertas = respostasCertas + 1
-            print("Restam %s pares para completar o jogo!" % (totalRespostasCertas - respostasCertas))
+            print("Restam %s pares para completar o jogo!" % (totalCombinacoes - respostasCertas))
         else:
             print("Você errou!")
     print("Jogo completado com sucesso!")
 
+
 def facil():
     print("---Iniciando jogo na dificuldade fácil---")
-    matriz4por4 = [['Z', 'A', 'Z', 'A'], ['X', 'O', 'P', 'Z'], ['N', 'P', 'M', 'N'], ['M', 'C', 'O', 'X']]
-    matriz4por4Oculta = [["#", "#", "#", "#"], ["#", "#", "#", "#"], ["#", "#", "#", "#"], ["#", "#", "#", "#"]]
+    matriz4por4 = criaMatriz(4, 4, 0)
+    matriz4por4Oculta = criaMatriz(4, 4, 1)
     imprimirMatrizOculta(matriz4por4Oculta)
-    iniciar(matriz4por4Oculta,matriz4por4,7)
+    iniciar(matriz4por4Oculta, matriz4por4)
 def medio():
     print("---Iniciando jogo na dificuldade média...---")
-    matriz8por8 = [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], ['Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X'],
-                   ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'], ['Y', 'Z', 'Z', 'B', 'Y', 'D', 'E', 'F'],
-                   ['G', 'N', 'P', 'I', 'I', 'P', 'G', 'N'], ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'],
-                   ['Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X'], ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']]
-    matriz8por8Oculta = [['#', '#', '#', '#', '#', '#', '#', '#'], ['#', '#', '#', '#', '#', '#', '#', '#'],
-                         ['#', '#', '#', '#', '#', '#', '#', '#'], ['#', '#', '#', '#', '#', '#', '#', '#'],
-                         ['#', '#', '#', '#', '#', '#', '#', '#'], ['#', '#', '#', '#', '#', '#', '#', '#'],
-                         ['#', '#', '#', '#', '#', '#', '#', '#'], ['#', '#', '#', '#', '#', '#', '#', '#']]
-
+    matriz8por8 = criaMatriz(8,8,0)
+    matriz8por8Oculta = criaMatriz(8,8,1)
     imprimirMatrizOculta(matriz8por8Oculta)
-    iniciar(matriz8por8Oculta,matriz8por8,26)
+    iniciar(matriz8por8Oculta, matriz8por8)
+
+def dificil():
+    print("---Iniciando jogo na dificuldade difícil...---")
+    matriz10por10 = criaMatriz(10, 10, 0)
+    matriz10por1Oculta = criaMatriz(10, 10, 1)
+    imprimirMatrizOculta(matriz10por1Oculta)
+    iniciar(matriz10por1Oculta, matriz10por10)
 
 dificuldade: int = int(input("Selecione a dificuldade. 1-Fácil, 2-Médio ou 3-Difícil"));
 if dificuldade == 1:
     facil()
 elif dificuldade == 2:
-   medio()
+    medio()
 else:
-    print("---Iniciando jogo na dificuldade difícil...---")
+    dificil()
